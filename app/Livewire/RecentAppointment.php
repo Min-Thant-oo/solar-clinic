baf49c407c9db58dad0d719b511e83bef4372daf
Min Thant Oo <minthantoo.ardil@gmail.com>
2024-09-24 13:20:09 -0500

    Admin & doctors can view appointments now

--- app/Livewire/RecentAppointment.php
+++ app/Livewire/RecentAppointment.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Appointment;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class RecentAppointment extends Component
 {
     #[Layout('layouts.app')]
+
+    public $recent_appointments;
+
+    public function mount() {
+        $this->recent_appointments = Appointment::with('doctor','patient')->orderBy('created_at', 'DESC')->limit(10)->get();
+    }
     
     public function render()
     {
